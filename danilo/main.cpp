713534b9e99e45e3456773bd085fd938bf7489e6
DaniloTato <Noentiendo2005@gmail.com>
2026-01-28 19:51:19 -0600

    Refactor project struct

--- danilo/main.cpp
+++ danilo/main.cpp
@@ -113,47 +113,45 @@ private:
   }
 };
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(500, 500), "Danilito");
-    window.setFramerateLimit(120);
+int main(){
+  sf::RenderWindow window(sf::VideoMode(500, 500), "Danilito");
+  window.setFramerateLimit(120);
 
-    Player player(
-      {0,0},
-      {15,15}
-    );
+  Player player(
+    {0,0},
+    {15,15}
+  );
 
-    Wall wall(
-      {100,100},
-      {50,50}
-    );
+  Wall wall(
+    {100,100},
+    {50,50}
+  );
 
-    Wall wall2(
-      {200,100},
-      {50,50}
-    );
+  Wall wall2(
+    {200,100},
+    {50,50}
+  );
 
-    while (window.isOpen())
+  while (window.isOpen()){
+    sf::Event event;
+    while (window.pollEvent(event))
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-          if (event.type == sf::Event::Closed) window.close();
-        }
-
-        player.movementX();
-        player.movementY();
-
-        // Window
-        window.clear();
-        player.render(window);
-
-        for(auto& wall : Wall::getS_Wall()){
-          wall ->render(window);
-        }
-
-        window.display();
+      if (event.type == sf::Event::Closed) window.close();
     }
 
-    return 0;
+    player.movementX();
+    player.movementY();
+
+    // Window
+    window.clear();
+    player.render(window);
+
+    for(auto& wall : Wall::getS_Wall()){
+      wall ->render(window);
+    }
+
+    window.display();
+  }
+
+  return 0;
 }
